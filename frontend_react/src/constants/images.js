5174ae6e02d645adcb6f9fe20ef84e07537f4137
johnitopaisah <johnitopaisah@gmail.com>
2024-11-11 16:43:21 +0100

    committing all changes

---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,19 @@
+import logo from '../assets/logo.png';
+import email from '../assets/email.png';
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import node from '../assets/node.png';
+import django from '../assets/django.png';
+import spring from '../assets/spring.png';
+import bgIMG from '../assets/bgIMG.png';
+
+export default {
+  email,
+  logo,
+  profile,
+  circle,
+  node,
+  django,
+  spring,
+  bgIMG,
+}
