32c19f72c5a91561ddf5b88a1fdc094b98e24e08
johnitopaisah <johnitopaisah@gmail.com>
2024-12-14 19:57:14 +0100

    updating the skill and experience

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,3 +1,35 @@
+// import logo from '../assets/logo.png';
+// import email from '../assets/email.png';
+// import profile from '../assets/profile.png';
+// import circle from '../assets/circle.svg';
+// import node from '../assets/node.png';
+// import django from '../assets/django.png';
+// import c_prog from '../assets/c_prog.png';
+// import spring from '../assets/spring.png';
+// import bgIMG from '../assets/bgIMG.png';
+// import about01 from '../assets/about01.png';
+// import about02 from '../assets/about02.png';
+// import about03 from '../assets/about03.png';
+// import about04 from '../assets/about04.png';
+// import mobile from '../assets/mobile.png';
+
+
+// export default {
+//   email,
+//   logo,
+//   profile,
+//   circle,
+//   node,
+//   django,
+//   spring,
+//   bgIMG,
+//   c_prog,
+//   about01,
+//   about02,
+//   about03,
+//   about04,
+//   mobile,
+// }
 import logo from '../assets/logo.png';
 import email from '../assets/email.png';
 import profile from '../assets/profile.png';
@@ -13,8 +45,7 @@ import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 import mobile from '../assets/mobile.png';
 
-
-export default {
+const images = {
   email,
   logo,
   profile,
@@ -29,4 +60,6 @@ export default {
   about03,
   about04,
   mobile,
-}
+};
+
+export default images;
